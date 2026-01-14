da0ed65c068b672a96ad5a15e32e08605877cd53
Roshan A <roshananand2005@gmail.com>
2026-01-14 21:59:48 +0530

    chore : added github handle

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,9 +1,25 @@
+import { FaGithub } from "react-icons/fa";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
 
 export function RootHeader() {
 	return (
-		<header className="flex items-center justify-between gap-4 px-2">
+		<header className="flex items-center gap-4 px-2">
 			<div className="font-bold">Sketchy.io</div>
+			<Button
+				variant={"outline"}
+				className="ml-auto"
+				onClick={() =>
+					window.open(
+						"https://github.com/Roshan-anand/sketchy_io",
+						"_blank",
+						"noopener,noreferrer",
+					)
+				}
+				aria-label="Open project on GitHub"
+			>
+				<FaGithub />
+			</Button>
 			<ModeToggle />
 		</header>
 	);
